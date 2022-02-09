6e09bd6e5cceeb1fcf9d17ba4700c48571cdfd88
Kathryn Thompson <katyounge@hotmail.com>
2022-02-09 15:21:11 -0800

    added explanatory comments and new if else functionality

--- dadjokes.js
+++ dadjokes.js
@@ -30,6 +30,10 @@ switch(weekDay) {
         break;
 }
 
+
+// inserts the date into the welcome message
+document.querySelector("#message1").textContent = message1;
+
 // declare variable for message 2
 let message2;
 
@@ -49,6 +53,10 @@ function chooseMessage2(child) {
         message2 = "Hello, My Baby Girl. Here is your joke for today: "
     }
 
+    else if (child == "jeremy") {
+        message2 = "Teachers like dad jokes? Ok-ay. Here you go: "
+    }
+
     else {
         message2 = "You're not one of my children but I'll joke you up anyway."
     }
@@ -56,34 +64,40 @@ function chooseMessage2(child) {
     return message2
 }
 
-
-
-
-document.querySelector("#message1").textContent = message1;
+// this tells the button what to do when it's clicked. it is created so it can be
+// passed as an argument into the EventListener
 
 function clickHandler() {
+    // grabs the value from the input box and inserts it into jokeChild variable
     let jokeChild = document.getElementById("fullNameBox").value;
     console.log(jokeChild);
+    // runs the chooseMessage2 function and puts it into the message2 variable
     message2 = chooseMessage2(jokeChild.toLowerCase());
+
+    // fetches the random joke by calling the getJoke function
     currentJoke = getJoke(message2);
     
 } 
 
+// this connects the button actions to the button element in the HTML
 buttonElement = document.querySelector("#submitName"); 
 buttonElement.addEventListener("click", clickHandler);
 
+// this resets the page and then prints out the joke on the screen with a personalized message
 function setJokeText(chosenMessage, chosenJoke){
     reset();
     document.querySelector('#message2').textContent = chosenMessage;
     document.querySelector('#joke_message').textContent = chosenJoke;
 };
 
+// this resets the page
 function reset() {
     document.querySelector('#message2').textContent = '';
     document.querySelector('#joke_message').textContent = '';
 
 };
 
+// this fetches the joke, pulls out the JSON, identifies the joke from the object, and sets the joke message
 function getJoke(message2) {
     
     let url = 'https://icanhazdadjoke.com/'
